2b541af34417a0da9d9afa4f207296eb078cd150
Praveen <pkumar@riffyn.com>
2019-01-15 22:12:07 -0800

    updated Jenkinsfile to use local build script which installs dependencies first before make

---
+++ riffyn/scripts/build-postgres.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# install dependencies
+apt-get update
+apt-get install -y \
+        curl \
+        procps \
+        sysstat \
+        libldap2-dev \
+        libpython-dev \
+        libreadline-dev \
+        libssl-dev \
+        bison \
+        flex \
+        libghc-zlib-dev \
+        libcrypto++-dev \
+        libxml2-dev \
+        libxslt1-dev \
+        bzip2 \
+        make \
+        gcc \
+        unzip \
+        python \
+        locales \
+        \
+        && rm -rf /var/lib/apt/lists/* \
+        && localedef -i en_US -c -f UTF-8 en_US.UTF-8 \
+
+apt-get update
+
+export LANG=en_US.utf8
+
+# configure the system for build
+./configure
+
+# build the system
+make
